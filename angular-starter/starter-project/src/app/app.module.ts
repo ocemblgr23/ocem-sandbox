892db0e99467d5e85bc14453d43ffb9e13e3f026
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-01-01 17:32:53 +0530

    angular starter project added with prime ng

---
+++ angular-starter/starter-project/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
