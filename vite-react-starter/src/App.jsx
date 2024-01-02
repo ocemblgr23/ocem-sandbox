fb21ab3e7a3d7dea776887a83e357f665f6c88dd
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-01-02 10:34:28 +0530

    prime react added and configure successfully done

--- vite-react-starter/src/App.jsx
+++ vite-react-starter/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
-
+import { Button } from 'primereact/button';
 function App() {
   return (
     <>
-      <h1>Prime React Starter Project (OCEM Sandbox)</h1>
+      <div className='card flex flex-column align-items-center py-4 justify-content-center'>
+        <h1>Prime React Starter Project (OCEM Sandbox)</h1>
+        <Button label='Check' icon='pi pi-check' />
+      </div>
     </>
   );
 }
