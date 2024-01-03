76e8a817253681ea143e767231b058e0b1c3a34a
ocemblgr23 <131694566+ocemblgr23@users.noreply.github.com>
2024-01-03 21:44:27 +0530

    update basic style for common css

--- javascript-sandbox/index.js
+++ javascript-sandbox/index.js
@@ -3,3 +3,12 @@ import { GREET } from './common/message';
 console.log('Hello World App');
 
 console.log(GREET);
+
+const greetDeepak = () => {
+  console.log("Hello Deepak Sir");
+}
+
+// Call the function
+greetDeepak()
+
+//  This is screen recording software for demo recording .... Stay tune for more videos
