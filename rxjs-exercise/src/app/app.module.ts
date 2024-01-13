6e7d7e0e8facad1dfdec9e95aacb959db029e0c3
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-01-13 19:36:29 +0530

    code clean up and docs link update readme

--- rxjs-exercise/src/app/app.module.ts
+++ rxjs-exercise/src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
