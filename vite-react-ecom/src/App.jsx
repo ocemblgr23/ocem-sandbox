a7140bb559c5b65ff7db3d4b75db4c466baaa727
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-01-15 17:19:41 +0530

    implement user search

--- vite-react-ecom/src/App.jsx
+++ vite-react-ecom/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Camera } from 'lucide-react';
 import Icon from './components/Icon';
 import * as Prime from './config/PrimeConfig';
@@ -6,8 +6,56 @@ import LandingPage from './pages/LandingPage';
 import Posts from './components/Posts';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  // Api call
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const resp = await fetch(usersUrl);
+      const data = await resp.json();
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handelChange = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
     <>
+      {/* Search and Filter */}
+      <div className='container'>
+        <div className='card py-3 flex align-items-center justify-content-center '>
+          <span className=' p-float-label p-input-icon-right'>
+            <i className='pi pi-spin pi-spinner' />
+            <Prime.InputText
+              onChange={handelChange}
+              placeholder='Search user'
+            />
+          </span>
+        </div>
+        <div className='card'>
+          <ul>
+            {users
+              .filter((user) =>
+                query.toLowerCase() === ''
+                  ? user
+                  : user.name.toLowerCase().includes(query.toLowerCase())
+              )
+              .map((user, index) => {
+                return <li key={index}>{user.name}</li>;
+              })}
+          </ul>
+        </div>
+      </div>
+
+      <Prime.Divider />
       <Posts />
       <LandingPage />
       {/* <div className='container'>
--- vite-react-ecom/src/config/PrimeConfig.jsx
+++ vite-react-ecom/src/config/PrimeConfig.jsx
@@ -3,5 +3,5 @@ import { Button } from 'primereact/button';
 import { Chip } from 'primereact/chip';
 import { Divider } from 'primereact/divider';
 import { ProgressSpinner } from 'primereact/progressspinner';
-
-export { Card, Button, Chip, Divider, ProgressSpinner };
+import { InputText } from 'primereact/inputtext';
+export { Card, Button, Chip, Divider, ProgressSpinner, InputText };
